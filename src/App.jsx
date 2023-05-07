2de4bbb3d517f6dfaaec125a39d17435a6cc66f6
zamanmayed1 <zamanmayed02@gmail.com>
2023-05-07 11:35:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='w-full h-[100vh] bg-[url("https://png.pngtree.com/background/20230216/original/pngtree-islamic-background-picture-image_2027687.jpg")] bg-cover flex justify-center items-center bg-no-repeat bg-fixed'>
+      <h1 className="text-5xl font-bold text-center text-blue-900 animate-bounce mt-7">
+        Welcome To Project: <span className="text-red-500"> Dashboard !</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
